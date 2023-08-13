d97b38fc8bd6a900ee4637b6a76352f52d593653
yuvrajdas <yuvrajdas1531@gmail.com>
2023-08-13 17:28:15 +0530

    Tree basics

---
+++ __3PreOrderBinaryTree.java
@@ -0,0 +1,42 @@
+public class __3PreOrderBinaryTree {
+    
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static class BinaryTree{
+        static int idx = -1;
+        public static Node constructTree(int nodes[]){
+            idx++;
+            if(nodes[idx] == -1) return null;
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = constructTree(nodes);
+            newNode.right = constructTree(nodes);
+            return newNode;
+        }
+
+        public static void preOrderTraversal(Node root){
+            if(root == null) return;
+
+            System.out.print(root.data+" ");
+            preOrderTraversal(root.left);
+            preOrderTraversal(root.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        BinaryTree bt = new BinaryTree();
+        int arr[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        Node root = bt.constructTree(arr);
+        bt.preOrderTraversal(root);
+    }
+}
