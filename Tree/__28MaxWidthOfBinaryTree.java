65562ff2d4e56aebec317dcf8234c70f99c22495
yuvraj <yuvrajdas1531@gmail.com>
2023-09-15 09:25:38 +0530

    width of binary Tree

--- Tree/__28MaxWidthOfBinaryTree.java
+++ Tree/__28MaxWidthOfBinaryTree.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class __28MaxWidthOfBinaryTree {
 
     public static class Node {
@@ -9,6 +11,16 @@ public class __28MaxWidthOfBinaryTree {
             this.data = data;
             this.left = null;
             this.right = null;
+        }
+    }
+    
+    public static class Pair{
+        Node mwNode;
+        int idx;
+
+        Pair(Node mwNode, int idx){
+            this.mwNode = mwNode;
+            this.idx = idx;
         }
     }
 
@@ -41,10 +53,27 @@ public class __28MaxWidthOfBinaryTree {
             printTree(root.left);
             printTree(root.right);
         }
-
         public static void widthOfBinaryTree(Node root){
-            if(root == null) return;
+           Queue<Pair> que = new ArrayDeque<>();
+           int max = 0;
+           que.add(new Pair(root, 0));
             
+           while(que.size()>0){
+            int count = que.size();
+            Pair leftTreeNode = que.peek();
+            int lmost = leftTreeNode.idx;
+            int rmost = leftTreeNode.idx;
+            for(int i=0; i<count; i++){
+                Pair rp = que.remove(); 
+                rmost = rp.idx;
+
+                if(rp.mwNode.left!=null) que.add(new Pair(rp.mwNode.left, 2*rp.idx+1));
+                
+                if(rp.mwNode.right!=null) que.add(new Pair(rp.mwNode.right, 2*rp.idx+2));
+            }
+            max = Math.max(max, rmost-lmost+1);
+           }
+           System.out.println(max);
         }
     }
 
@@ -52,9 +81,9 @@ public class __28MaxWidthOfBinaryTree {
         BinaryTree bt = new BinaryTree();
         int arr[] = { 1, 3, 5, -1, -1, 3, -1, -1, 2, -1, 9, -1, -1 };
         Node root = bt.constructTree(arr);
-        bt.printTree(root);
+        // bt.printTree(root);
 
         bt.widthOfBinaryTree(root);
-
+        
     }
 }
