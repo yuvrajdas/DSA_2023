db3b048757f73bad1213aeaa2f243b5f5fec5cca
yuvraj <yuvrajdas1531@gmail.com>
2023-09-15 15:25:05 +0530

    Vertical Order Traversal Method 2

---
+++ Tree/__30VerticalOrderTraversalOfBinaryTree2.java
@@ -0,0 +1,18 @@
+public class __30VerticalOrderTraversalOfBinaryTree2{
+    
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+            this.left = this.right = null;
+        }
+    }
+    
+    
+    public static void main(String[] args) {
+        
+    }
+}
