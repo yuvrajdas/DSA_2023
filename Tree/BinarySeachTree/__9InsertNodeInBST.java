8c1e7db80ef6afb42140f17bbc2eab502bf68396
yuvraj <yuvrajdas1531@gmail.com>
2023-09-19 11:41:05 +0530

    Insert Node in BST

--- Tree/BinarySeachTree/__9InsertNodeInBST.java
+++ Tree/BinarySeachTree/__9InsertNodeInBST.java
@@ -1,39 +1,40 @@
 package BinarySeachTree;
 
 public class __9InsertNodeInBST {
-    public static class Node{
+    public static class Node {
         int data;
         Node left;
         Node right;
 
-        Node(int data, Node left, Node right){
+        Node(int data, Node left, Node right) {
             this.data = data;
             this.left = left;
             this.right = right;
         }
 
-        Node(int data){
+        Node(int data) {
             this.data = data;
             this.left = null;
             this.right = null;
         }
     }
 
-    public static class BinarySearchTree{
+    public static class BinarySearchTree {
 
-        public static Node constructBST(int nodes[], int si, int ei){
-            if(si>ei) return null;
+        public static Node constructBST(int nodes[], int si, int ei) {
+            if (si > ei)
+                return null;
 
-            int mid = si+(ei-si)/2;
+            int mid = si + (ei - si) / 2;
             int data = nodes[mid];
 
-            Node leftCNode = constructBST(nodes, si, mid-1);
-            Node rightCNode = constructBST(nodes, mid+1, ei);
+            Node leftCNode = constructBST(nodes, si, mid - 1);
+            Node rightCNode = constructBST(nodes, mid + 1, ei);
 
             Node root = new Node(data, leftCNode, rightCNode);
             return root;
         }
- 
+
         public static void printBST(Node root) {
             if (root == null)
                 return;
@@ -47,13 +48,27 @@ public class __9InsertNodeInBST {
             printBST(root.left);
             printBST(root.right);
         }
+
+        public static Node InsertInBST(Node root, int data) {
+            if (root == null)
+                return new Node(data);
+
+            if (data > root.data) {
+                root.right = InsertInBST(root.right, data);
+            } else {
+                root.left = InsertInBST(root.left, data);
+            }
+
+            return root;
+        }
     }
 
     public static void main(String[] args) {
         int arr[] = { 1, 2, 3, 4, 5, 6, 7 };
         Node root = BinarySearchTree.constructBST(arr, 0, arr.length - 1);
         BinarySearchTree.printBST(root);
-        
+        Node nRoot = BinarySearchTree.InsertInBST(root, 0);
+        BinarySearchTree.printBST(nRoot);
 
     }
 }
